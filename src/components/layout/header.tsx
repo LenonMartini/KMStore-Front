51b73404ac7069cda21d1ba770ca0771275891fe
lenonmartini <lenonmartini21gmail.com>
2025-08-26 13:30:59 -0300

    Iniciando o Component Header

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,10 @@
+export function Header(){
+    return (
+        <header>
+            <div className="bg-black text-white text-center p-4">
+                <strong>FRETE GRÁTIS</strong> para todos o Paraná nas compras acima de R$ 199,00. APROVEITA!
+            </div>
+            ....
+        </header>
+    )
+}
